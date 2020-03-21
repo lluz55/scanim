15f9d6bc586f322ced885ccccee0cdcff9045644
lluz55 <lucasluz55@gmail.com>
2020-03-21 11:31:00 -0300

    added async example

--- examples/watcher.nim
+++ examples/watcher.nim
@@ -1,9 +1,26 @@
-import scanim
+import ../scanim
+import threadpool
 
 proc cb(change: WatchStatus) =
     echo change
+    stdout.write("> ")
 
 
-watchFiles:
-    watchFile("test.txt", cb)
-    watchFile("test2.txt", cb)
+# Sync
+# watchFiles:
+#     watchFile("test.txt", cb)
+#     watchFile("test2.txt", cb)
+
+# watchFile("test.txt", cb)
+# watchFile("test2.txt", cb)
+
+# Async
+var stm = spawn stdin.readLine()
+stdout.write("> ")
+watchFilesLoop(1000) do:
+    if stm.isReady:
+        watchFile(^stm, cb)
+        stm = spawn stdin.readLine()
+        stdout.write("> ")
+
+
